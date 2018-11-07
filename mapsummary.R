d23cd40b77856f1583d183e55278a656c997f7b1
jcvdav <juancarlos.villader@gmail.com>
2018-11-07 08:50:59 -0800

    Deleted the use of setwd and used here::here to specify file location

--- mapsummary.R
+++ mapsummary.R
@@ -12,11 +12,9 @@ library(ggplot2)
 
 ##Import world map file and cropping to the Caribbean Region
 
-setwd("/Users/jmlawson/github/kuni_fads")
+fadlist <- read_csv(here::here("raw_data", "faddata.csv"))
 
-fadlist <- read_csv('faddata.csv')
-
-map.world<-map_data("world")
+map.world <- map_data("world")
 
 map.world_joined_fad <- left_join(map.world, fadlist, by = c('region' = 'region'))
 View(map.world_joined_fad)
