c9c8d7a40d7c8a87b3937521cd7e91678444754c
MollyWilson <mwwilson@ucsb.edu>
2019-10-09 15:42:30 -0700

    updating nutrition file

--- scripts/processing/nutrition_wgi.R
+++ scripts/processing/nutrition_wgi.R
@@ -9,15 +9,6 @@ library(countrycode)
 iso <- read.csv(here("raw_data", "iso_codes.csv"), stringsAsFactors = F)
 
 ### nutrition + food security datasets
-fao_intake <- read.csv(here("raw_data", "nutrition", "fao_intake.csv"), stringsAsFactors = F) %>% 
-  clean_names() %>%
-  filter(item == "Pelagic Fish", year == 2013, country != "Caribbean") %>% ### eventually calculate 3-year average?
-  select(country,element,value) %>%
-  spread(element,value) %>%
-  set_names("country","fao_fat_pf","fao_cal_pf","fao_prot_pf") %>%
-  mutate(alpha_3 = countrycode(country, 'country.name', 'iso3c')) %>%
-  select(alpha_3, everything(), -country)
-
 fao_fs <- fao_fs <- read.csv(here("raw_data", "nutrition", "fao_fs_indicators.csv"), stringsAsFactors = F) %>% 
   clean_names() %>%
   filter(area != "Caribbean") %>%
@@ -29,19 +20,27 @@ fao_fs <- fao_fs <- read.csv(here("raw_data", "nutrition", "fao_fs_indicators.cs
 
 genus_intake <- read.csv(here("raw_data", "nutrition", "genus_intake.csv"), stringsAsFactors = F) %>% 
   clean_names() %>% # all from 2011
-  select(country,calories_pelagicfish,fat_pelagicfish,protein_pelagic_fish) %>%
-  set_names("country","gen_cal_pf","gen_fat_pf","gen_prot_pf") %>%
+  mutate(calories_pf = calories_pelagicfish / calories,
+         protein_pf = protein_pelagic_fish / protein) %>%
+  select(country,calories_pf,protein_pf) %>%
+  mutate(country= ifelse(country == "Netherlands Antilles", "Bonaire, Sint Eustatius and Saba", country)) %>% 
   mutate(alpha_3 = countrycode(country, 'country.name', 'iso3c')) %>%
   select(alpha_3, everything(), -country)
 
-intake <- fao_intake %>%
-  left_join(genus_intake, by = "alpha_3")
-# same coverage, and FAO is more recent (2013) - so use that
+fao_intake <- read.csv(here("raw_data", "nutrition", "fao_intake.csv"), stringsAsFactors = F) %>% 
+  clean_names() %>%
+  filter(item == "Pelagic Fish", year == 2013, country != "Caribbean") %>% ### eventually calculate 3-year average?
+  select(country,element,value) %>%
+  spread(element,value) %>%
+  set_names("country","fao_fat_pf","fao_cal_pf","fao_prot_pf") %>%
+  mutate(alpha_3 = countrycode(country, 'country.name', 'iso3c')) %>%
+  select(alpha_3, everything(), -country)
+# currently not using FAO intake data because GENuS has total values for calculating percent intake
 
 # selected nutrition variables:
 nutrition <- fao_fs %>%
   select(alpha_3, energy_adequacy) %>% 
-  full_join(fao_intake, by = "alpha_3")
+  full_join(genus_intake, by = "alpha_3")
 
 ### governance indicators
 wgi <- read.csv(here("raw_data", "governance", "wgi_indicators.csv"), stringsAsFactors = F) %>%
