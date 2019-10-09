b95ebfdffa1c53b8258f4ba4b87962e321584c57
MollyWilson <mwwilson@ucsb.edu>
2019-10-09 10:53:05 -0700

    updating nutrition/wgi processing

--- scripts/processing/nutrition_wgi.R
+++ scripts/processing/nutrition_wgi.R
@@ -3,6 +3,7 @@
 library(tidyverse)
 library(here)
 library(janitor)
+library(countrycode)
 
 # file with country names and alpha_3 codes
 iso <- read.csv(here("raw_data", "iso_codes.csv"), stringsAsFactors = F)
@@ -13,23 +14,34 @@ fao_intake <- read.csv(here("raw_data", "nutrition", "fao_intake.csv"), stringsA
   filter(item == "Pelagic Fish", year == 2013, country != "Caribbean") %>% ### eventually calculate 3-year average?
   select(country,element,value) %>%
   spread(element,value) %>%
-  set_names("country","fao_fat_pf","fao_cal_pf","fao_prot_pf")
+  set_names("country","fao_fat_pf","fao_cal_pf","fao_prot_pf") %>%
+  mutate(alpha_3 = countrycode(country, 'country.name', 'iso3c')) %>%
+  select(alpha_3, everything(), -country)
 
 fao_fs <- fao_fs <- read.csv(here("raw_data", "nutrition", "fao_fs_indicators.csv"), stringsAsFactors = F) %>% 
   clean_names() %>%
   filter(area != "Caribbean") %>%
   select(area,item,value) %>%
   spread(item, value) %>%
-  set_names("country","energy_adequacy","sev_insecurity","undernourishment")
+  set_names("country","energy_adequacy","sev_insecurity","undernourishment") %>%
+  mutate(alpha_3 = countrycode(country, 'country.name', 'iso3c')) %>%
+  select(alpha_3, everything(), -country)
 
 genus_intake <- read.csv(here("raw_data", "nutrition", "genus_intake.csv"), stringsAsFactors = F) %>% 
   clean_names() %>% # all from 2011
   select(country,calories_pelagicfish,fat_pelagicfish,protein_pelagic_fish) %>%
-  set_names("country","gen_cal_pf","gen_fat_pf","gen_prot_pf")
+  set_names("country","gen_cal_pf","gen_fat_pf","gen_prot_pf") %>%
+  mutate(alpha_3 = countrycode(country, 'country.name', 'iso3c')) %>%
+  select(alpha_3, everything(), -country)
 
 intake <- fao_intake %>%
-  left_join(genus_intake, by = "country")
-### same coverage, and FAO is more recent (2013) - so use that
+  left_join(genus_intake, by = "alpha_3")
+# same coverage, and FAO is more recent (2013) - so use that
+
+# selected nutrition variables:
+nutrition <- fao_fs %>%
+  select(alpha_3, energy_adequacy) %>% 
+  full_join(fao_intake, by = "alpha_3")
 
 ### governance indicators
 wgi <- read.csv(here("raw_data", "governance", "wgi_indicators.csv"), stringsAsFactors = F) %>%
@@ -38,7 +50,10 @@ wgi <- read.csv(here("raw_data", "governance", "wgi_indicators.csv"), stringsAsF
   select(country_name, country_code, series_name, value) %>%
   spread(series_name, value) %>%
   set_names("country","alpha_3","corruption","gov_eff","pol_stab","reg_qual","rule_law","accountability") %>%
-  mutate(wgi_mean = mean(corruption, gov_eff, pol_stab, reg_qual, rule_law, accountability))
+  mutate_at(vars(3:8),funs(as.numeric)) %>%
+  rowwise() %>%
+  mutate(wgi_mean = mean(c(corruption, gov_eff, pol_stab, reg_qual, rule_law, accountability), na.rm = T))
+  select(alpha_3, everything(), -country)
 
 
 
