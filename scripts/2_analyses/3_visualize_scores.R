91a764b97debd36846cff760322c3a28afa7e669
ignacia_rivera <m_i_rivera@ucsb.edu.com>
2019-11-21 18:54:08 -0500

    changing the names of the scores from econ to markeatability and from nutrition to social need

--- scripts/2_analyses/3_visualize_scores.R
+++ scripts/2_analyses/3_visualize_scores.R
@@ -38,7 +38,7 @@ ggsave(filename = here("img", "score_pair_plot.png"),
 
 # Plot the data
 data %>% 
-  mutate(score_econ = ifelse(is.na(score_econ), 0, score_econ)) %>%
+  mutate(score_marketability = ifelse(is.na(score_marketability), 0, score_marketability)) %>%
   mutate(score_govt_breaks = case_when(score_govt <= quantile(score_govt, 1/3, na.rm = T) ~ "Low",
                                        score_govt >= quantile(score_govt, 2/3, na.rm = T) ~ "High",
                                        between(score_govt,
@@ -47,9 +47,9 @@ data %>%
                                        T ~ "NA"),
          score_govt_breaks = fct_relevel(score_govt_breaks,
                                          "Low", "Med", "High", "NA")) %>% 
-  ggplot(aes(x = score_cost, y = score_econ)) +
+  ggplot(aes(x = score_cost, y = score_marketability)) +
   geom_point(size = 1, shape = 21, fill = "gray") +
-  geom_point(shape = 21, aes(size = score_nutrit, fill = score_govt_breaks)) +
+  geom_point(shape = 21, aes(size = score_need, fill = score_govt_breaks)) +
   geom_hline(yintercept = 0.5, linetype = "dashed") +
   geom_vline(xintercept = 0.5, linetype = "dashed") +
   geom_label_repel(aes(label = ISO3),
@@ -57,7 +57,7 @@ data %>%
                    point.padding = 0.7,
                    size = 2,
                    min.segment.length = 0) +
-  startR::ggtheme_plot() +
+  #startR::ggtheme_plot() +
   scale_fill_manual(values = c("red", "orange", "darkgreen", "gray")) +
   scale_size_continuous(breaks = c(0.3, 0.6, 1), range = c(1, 10)) +
   labs(x = "Biophysical cost",
