5e89d4d32ca79ca9fe53e3a1d51fbc31b811835a
Molly Wilson <mwattswilson@gmail.com>
2020-07-20 15:26:50 -0400

    updating map

--- scripts/2_analyses/fad_numbers.R
+++ scripts/2_analyses/fad_numbers.R
@@ -51,6 +51,33 @@ points_now <- coast %>%
 
 # Create plot
 
+ggtheme_map <- function(base_size = 9) {
+  theme(
+    text = element_text(#family = "Helvetica",
+      color = "gray30",
+      size = base_size),
+    plot.title = element_text(
+      size = rel(1.25),
+      hjust = 0,
+      face = "bold"),
+    panel.background = element_blank(),
+    legend.position = "right",
+    strip.background = element_rect(
+      fill = "transparent"),
+    panel.border = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.grid.major = element_line(
+      color = "transparent"),
+    axis.ticks = element_blank(),
+    axis.text = element_blank(),
+    axis.title = element_blank(),
+    legend.key = element_rect(
+      colour = NA,
+      fill = NA),
+    axis.line = element_blank()
+  )
+}
+
 ## MAP BEFORE
 map_before <- ggplot() +
   geom_sf(data = coast, fill = "lightgray", color = "black") +
@@ -97,8 +124,8 @@ map <- ggplot() +
                      seed = 1) +
   ggtheme_map() +
   scale_size_continuous(range = c(1, 6), breaks = c(1, 10, 100, 1000)) +
-  guides(size = guide_legend(title = "Number of FADs")) +
-  ggtitle("Estimated MFAD numbers (present)") +
+  guides(size = guide_legend(title = "Number of MFADs")) +
+  ggtitle("") +
   theme(legend.justification = c(1, 1),
         legend.position = c(1, 1))
 
