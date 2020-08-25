0e82bc06e0bb40b4cd05a217748da3870ca49468
Molly Wilson <mwwilson@ucsb.edu>
2020-08-25 08:40:46 -0400

    updating timeline fig

--- scripts/2_analyses/fad_timeline.R
+++ scripts/2_analyses/fad_timeline.R
@@ -17,11 +17,8 @@ timeline <- read.csv(here("raw_data", "fad_data", "fads_timeline.csv"),
 
 ggplot(timeline, aes(x = year, y = reorder(name, desc(name)))) +
   geom_point(aes(size = n_fads, shape = presence), alpha = 0.5, color = "darkslategray4", height = 0, width = 0.5) +
-  #scale_size(breaks = c(0,100,500)) +
-  scale_size_continuous(range = c(1,10)) +
-  #scale_colour_manual(values = c("darkslategray4","darkslategray4")) +
+  scale_size_continuous(range = c(1,10), labels = c("0", "100", "200", "300", "400", "500 +")) +
   scale_shape_manual(values = c(21,16)) +
-  #scale_alpha_manual(values = c(1,.5)) +
   theme_bw() +
   labs(x = "", y = "", shape = "MFAD Presence", size = "Num. MFADs")
 
