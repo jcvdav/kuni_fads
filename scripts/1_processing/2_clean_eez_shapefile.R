4803832071dafa5a04816c203fb5695b07d7979b
Juan Carlos Villasenor Derbez <juancarlos@ucsb.edu>
2020-08-26 13:35:02 -0700

    Separated BES into ESS (Eustatius and Saba) and BON (Bonaire)

--- scripts/1_processing/2_clean_eez_shapefile.R
+++ scripts/1_processing/2_clean_eez_shapefile.R
@@ -21,8 +21,11 @@ iso <- read.csv(here("raw_data", "iso_codes.csv"),
   pull(alpha_3)
 
 # Load EEZ
-eez <- st_read(here("raw_data", "eez")) %>% 
+eez <- st_read(here("raw_data", "eez_v10")) %>% 
   filter(ISO_Ter1 %in% c(iso, "BMU")) %>%             # Filtering ISO 3 codes of interest
+  mutate(ISO_Ter1 = case_when(MRGID_Ter1 %in% c(19095, 26525) ~ "ESS",
+                              MRGID_Ter1 == 15074 ~ "BON",
+                              T ~ ISO_Ter1)) %>% 
   group_by(ISO_Ter1) %>% 
   summarize(a = 0) %>% 
   select(ISO_Ter1) %>%                      # Keeping only column of interest
