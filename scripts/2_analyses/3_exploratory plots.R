141a2c2510ed0d8c6f1cc629de7a48445512f7fd
Molly Wilson <mwwilson@ucsb.edu>
2021-06-10 09:15:49 -0400

    working on looking at public vs. private dynamics

--- scripts/2_analyses/3_exploratory plots.R
+++ scripts/2_analyses/3_exploratory plots.R
@@ -10,7 +10,8 @@ library(tidyverse)
 
 scaled_data <- read.csv(here("data", "data_scaled.csv"),
                         stringsAsFactors = F) %>%
-  select(alpha_3, score_govt, score_wgi, score_need, score_marketability, n_fads, fads_per_totvessel)
+  select(alpha_3, score_govt, score_wgi, score_need, score_marketability, n_fads, n_private, fads_per_totvessel) %>%
+  mutate(p_private = n_private/n_fads)
 
 cost_data <- read.csv(here("data", "country_level_cost_summary_statistics.csv"),
                       stringsAsFactors = F) %>% 
@@ -26,9 +27,12 @@ data <- cost_data %>%
 
 ## Preliminary - comparing our 2 governance indicators - WGI vs. survey data (for islands with both)
 ggplot(data, aes(x = score_wgi, y = score_govt)) +
-  geom_text(aes(label=ISO3), size = 2) +
+  geom_point(aes(size = n_fads, color = p_private)) +
+  # geom_text(aes(label=ISO3), size = 2) +
   theme_bw()
-# seems mostly positively correlated, except for some countries that don't have FAD regulations bc they don't have FADs (ABW, BHS) but actually have high governance capacity
+# can see some countries that don't have FAD regulations bc they don't have FADs (ABW, BHS) but actually have high governance capacity
+# maybe instead of number of FADs here, classify as no fads, public, private?
+
 
 ## Cost and governance
 
@@ -44,11 +48,22 @@ ggplot(data, aes(x = score_cost, y = score_govt)) +
   geom_smooth(method = "lm") +
   theme_bw()
 
+### Cost ~ WGI ~ FADs
+ggplot(data, aes(x = score_cost, y = score_wgi)) +
+  geom_point(aes(size = fads_per_totvessel)) +
+  theme_bw()
+
+
 ## FAD numbers (FADs per total number of SSF vessels)
 # note - maybe FADs per km coastline would be more informative...?
 
 ### FADs ~ WGI
 ggplot(data, aes(x = score_wgi, y = fads_per_totvessel)) +
+  geom_text(aes(label=ISO3), size = 2) +
+  ylim(0,.75) +
+  theme_bw() 
+
+ggplot(data, aes(x = score_govt, y = fads_per_totvessel)) +
   geom_text(aes(label=ISO3), size = 2) +
   ylim(0,.75) +
   theme_bw() 
@@ -71,6 +86,14 @@ ggplot(data, aes(x = score_need, y = fads_per_totvessel)) +
   geom_text(aes(label=ISO3), size = 2) +
   ylim(0,.75) +
   theme_bw() 
+
+
+## Governance and public vs. private
+ggplot(data, aes(x = score_govt, y = p_private)) +
+  # geom_point(aes(size = n_fads, color = p_private)) +
+  geom_text(aes(label=ISO3), size = 2) +
+  theme_bw()
+
 
 ## Need 
 
